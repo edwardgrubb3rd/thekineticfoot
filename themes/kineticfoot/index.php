954dd81829a41b983e574b6b9df48bff88f5b60e
trevans24 <trevans24@gmail.com>
2018-07-31 19:03:26 -0600

    fixed navbar

--- themes/kineticfoot/index.php
+++ themes/kineticfoot/index.php
@@ -1,13 +1,35 @@
 <?php
 
+	/*
+	Template Name: Home
+	*/
+
  get_template_part( 'templates/header' );
 
 ?>
 
 <body>
 
-  <div class="hero">
-    hero here
+	<?php
+
+		$hero = get_field('hero_image');
+
+	?>
+
+  <div class="hero" style="background-image: url(<?php echo $hero ?>)">
+
+		<?php
+			$hero_content = get_field('hero_content');
+		 ?>
+
+		 <div class="hero-content">
+ 			<h1 class="title"><?php echo $hero_content['heading'] ?></h1>
+ 			<span class="subtitle">
+ 				<?php echo $hero_content['subtitle'] ?>
+ 			</span>
+			<a href="<?php echo $hero_content['button_link'] ?>" class="cta-button"><?php echo $hero_content['button_text'] ?></a>
+ 		</div>
+
   </div>
 
 </body>
