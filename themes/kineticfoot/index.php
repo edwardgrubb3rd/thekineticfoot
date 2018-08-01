c2c2bfc4cd37ba95ad7f14fab0653b897a54776c
trevans24 <trevans24@gmail.com>
2018-08-01 16:42:04 -0600

    about us section

--- themes/kineticfoot/index.php
+++ themes/kineticfoot/index.php
@@ -27,10 +27,40 @@
  			<span class="subtitle">
  				<?php echo $hero_content['subtitle'] ?>
  			</span>
-			<a href="<?php echo $hero_content['button_link'] ?>" class="cta-button"><?php echo $hero_content['button_text'] ?></a>
+			<a href="<?php echo $hero_content['button_link'] ?>" class="cta-button" target="_blank"><?php echo $hero_content['button_text'] ?></a>
  		</div>
 
   </div>
+
+	<!-- About Section -->
+	<?php
+		$about = get_field('about_title');
+	 ?>
+
+	<div class="about section">
+		<span class="title"><?php echo $about ?></span>
+
+		<div class="about-grid">
+
+			<?php
+			if (have_rows('about_us')):
+				while(have_rows('about_us')) : the_row();
+
+				$img = get_sub_field('image');
+        $subtitle = get_sub_field('subtitle');
+        $desc = get_sub_field('description');
+			?>
+
+			<div class="about-item">
+				<img class="about-item-img" src="<?php echo $img ?>" alt="">
+				<span class="about-item-subtitle"><?php echo $subtitle ?></span>
+				<p class="about-item-desc"><?php echo $desc ?></p>
+			</div>
+
+		<?php endwhile; endif; ?>
+
+		</div>
+	</div>
 
 </body>
 
