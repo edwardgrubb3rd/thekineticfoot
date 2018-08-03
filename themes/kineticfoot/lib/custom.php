f226ca5abc90e2f7b87ab31b9709497ed1912a6c
trevans24 <trevans24@gmail.com>
2018-08-03 16:36:19 -0600

    updated to use react

--- themes/kineticfoot/lib/custom.php
+++
@@ -1,8 +0,0 @@
-<?php //Custom code
-
-// allow upload of SVG files
-function cc_mime_types($mimes) {
-  $mimes['svg'] = 'image/svg+xml';
-  return $mimes;
-}
-add_filter('upload_mimes', 'cc_mime_types');
