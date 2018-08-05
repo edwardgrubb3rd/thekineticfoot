d54567eabf6f17282762f8ea948f29d8e38324f3
trevans24 <trevans24@gmail.com>
2018-08-05 17:59:24 -0600

    added contact section, and footer

--- client/src/components/global/Footer.js
+++ client/src/components/global/Footer.js
@@ -5,6 +5,9 @@ const Footer = (props) => (
     <div className="footer-section">
       <span className="title">The Kinetic Foot & Ankle Clinic</span>
       <span className="copyright">&copy; 2018 The Kinetic Foot & Ankle Clinic</span>
+      <div className="social-contact">
+
+      </div>
     </div>
     <div className="footer-section">
 
