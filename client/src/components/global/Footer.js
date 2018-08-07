730828604ee3ce787debc965c7372062677ef51a
trevans24 <trevans24@gmail.com>
2018-08-06 18:57:46 -0600

    more updates to theme

--- client/src/components/global/Footer.js
+++ client/src/components/global/Footer.js
@@ -9,10 +9,28 @@ const Footer = (props) => (
   <div className="footer section">
     <div className="footer-section left">
       <span className="title">The Kinetic Foot & Ankle Clinic</span>
-      <span className="copyright">&copy; 2018 The Kinetic Foot & Ankle Clinic</span>
+      <div className="footer-contact">
+        {
+          props.contact_info ?
+          <Fragment>
+            <span>{props.contact_info.street_address}</span>
+            <div className="city-state">
+              <span>{props.contact_info.city}, &nbsp;</span>
+              <span>{props.contact_info.state}</span>
+            </div>
+            <span>{props.contact_info.zip}</span>
+            <span>Phone: {props.contact_info.phone}</span>
+            <span>Fax: {props.contact_info.fax}</span>
+          </Fragment>
+          :
+          null
+        }
+      </div>
+    </div>
+    <div className="footer-section right">
       {
         props.social_media ?
-        <div className="social-contact" style={{width: `calc(${props.social_media.length} * 10%)`}}>
+        <div className="social-contact" style={{width: `calc(${props.social_media.length} * 50px)`}}>
           {
             props.social_media.map(({link, social_app}) => (
               <a href={link} key={social_app} className="social-icon">
@@ -24,25 +42,7 @@ const Footer = (props) => (
           :
           null
         }
-    </div>
-    <div className="footer-section right">
-      <div className="footer-contact">
-        {
-          props.contact_info ?
-          <Fragment>
-            <span>{props.contact_info.street_address}</span>
-            <div className="city-state">
-              <span>{props.contact_info.city}, &nbsp;</span>
-              <span>{props.contact_info.state}&nbsp;</span>
-              <span>{props.contact_info.zip}</span>
-            </div>
-            <span>Phone: {props.contact_info.phone}</span>
-            <span>Fax: {props.contact_info.fax}</span>
-          </Fragment>
-          :
-          null
-        }
-      </div>
+      <span className="copyright">&copy; 2018 The Kinetic Foot & Ankle Clinic</span>
     </div>
   </div>
 );
