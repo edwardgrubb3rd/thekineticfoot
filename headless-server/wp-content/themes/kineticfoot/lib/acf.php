80fba5d5e34221c94d1aac5b41b1f0a75b647cf0
trevans24 <trevans24@gmail.com>
2018-08-08 20:39:33 -0600

    removed wp server

--- headless-server/wp-content/themes/kineticfoot/lib/acf.php
+++
@@ -1,5 +0,0 @@
-<?php /* Custom ACF functions */
-// Enable ACF Options page
-if( function_exists( 'acf_add_options_page' ) ) {
-    acf_add_options_page();
-}
--- headless-server/wp-content/themes/kineticfoot/templates/sidebar.php
+++
@@ -1,5 +0,0 @@
-<?php if ( is_active_sidebar( 'sidebar-primary' ) ) : ?>
-	<aside class="sidebar">
-		<?php dynamic_sidebar( 'sidebar-primary' ); ?>
-	</aside>
-<?php endif;
