09867b4e2068961c206bc82eb99a3fc83ac8e80f
trevans24 <trevans24@gmail.com>
2018-08-08 18:41:51 -0600

    hero overlay

--- client/src/components/global/Hero.js
+++ client/src/components/global/Hero.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Hero = (props) => (
   <div className="hero" style={{backgroundImage: `url(${props.img})`}}>
+    <div className="hero-overlay"></div>
 
      <div className="hero-content">
       <h1 className="title">{props.title}</h1>
