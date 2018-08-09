1c89444dfa35366e91d14b06f579f7038a202885
trevans24 <trevans24@gmail.com>
2018-08-08 21:05:11 -0600

    update for deploy

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './style.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { DataProvider } from './context/DataContext';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
+  <DataProvider>
     <Router basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
+  </DataProvider>
   , document.getElementById('root'));
 registerServiceWorker();
