85c43bdf3effd5fafd32861dc8cff897ad4c6b97
trevans24 <trevans24@gmail.com>
2018-08-13 22:19:14 -0600

    removed packages

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const xoauth2 = require('xoauth2');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
