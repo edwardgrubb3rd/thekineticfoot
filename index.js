7b2090c9de15ecc5c361b233f8c4324e1843630d
trevans24 <trevans24@gmail.com>
2018-09-08 11:15:35 -0600

    more mobile fixes

--- index.js
+++ index.js
@@ -202,7 +202,7 @@ app.get('/api/pages/treatments', (req, res) => {
       {
         "name": "Achilles Tendon Disorders",
         "id": "achilles-tendon-disorders",
-        "description": "Achilles tendonitis is an inflammation of the Achilles tendon. This inflammation is typically short-lived. Over time, if not resolved, the condition may progress to a degeneration of the tendon (Achilles tendonosis), in which the tendon loses its organized structure and is likely to develop microscopic tears."
+        "description": "Achilles tendonitis is an inflammation of the Achilles tendon. Over time, if not resolved, the condition may progress to a degeneration of the tendon (Achilles tendonosis), in which the tendon loses its organized structure and is likely to develop microscopic tears."
       },
       {
         "name": "Ankle Fractures",
@@ -222,12 +222,12 @@ app.get('/api/pages/treatments', (req, res) => {
       {
         "name": "Chronic Ankle Instability",
         "id": "chronic-ankle-instability",
-        "description": "Chronic ankle instability is a condition characterized by a recurring giving way of the outer (lateral) side of the ankle. This condition often develops after repeated ankle sprains. Usually, the giving way occurs while walking or doing other activities, but it can also happen when you’re just standing. Many athletes, as well as others, suffer from chronic ankle instability."
+        "description": "Chronic ankle instability is a condition characterized by a recurring giving way of the outer (lateral) side of the ankle. This condition often develops after repeated ankle sprains."
       },
       {
         "name": "Diabetic Complications",
         "id": "diabetes",
-        "description": "People living with diabetes are prone to having foot problems, often because of two complications of diabetes: nerve damage (neuropathy) and poor circulation. Neuropathy causes loss of feeling in your feet, taking away your ability to feel pain and discomfort, so you may not detect an injury or irritation."
+        "description": "People living with diabetes are prone to having foot problems, often because of two complications of diabetes: nerve damage (neuropathy) and poor circulation."
       },
       {
         "name": "Ganglion Cyst",
@@ -247,7 +247,7 @@ app.get('/api/pages/treatments', (req, res) => {
       {
         "name": "Peroneal Tendon Injuries",
         "id": "peroneal-tendon-injuries",
-        "description": "The two peroneal tendons in the foot run side by side behind the outer ankle bone. One peroneal tendon attaches to the outer part of the midfoot, while the other tendon runs under the foot and attaches near the inside of the arch. The main function of the peroneal tendons is to stabilize the foot and ankle and protect them from sprains."
+        "description": "The two peroneal tendons in the foot run side by side behind the outer ankle bone. The main function of the peroneal tendons is to stabilize the foot and ankle and protect them from sprains."
       },
       {
         "name": "Plantar Fasciitis",
@@ -257,22 +257,22 @@ app.get('/api/pages/treatments', (req, res) => {
       {
         "name": "Posterior Tibial Tendon Dysfunction (PTTD)",
         "id": "pttd",
-        "description": "The posterior tibial tendon serves as one of the major supporting structures of the foot, helping it to function while walking. Posterior tibial tendon dysfunction (PTTD) is a condition caused by changes in the tendon, impairing its ability to support the arch. This results in flattening of the foot."
+        "description": "Posterior tibial tendon dysfunction (PTTD) is a condition caused by changes in the tendon, impairing its ability to support the arch. This results in flattening of the foot."
       },
       {
         "name": "Pre-dislocation Syndrome (Capsulitis)",
         "id": "capsulitis",
-        "description": "Capsulitis is a condition in which these ligaments have become inflamed. Although capsulitis can also occur in the joints of the third or fourth toes, it most commonly affects the second toe. This inflammation causes considerable discomfort and, if left untreated, can eventually lead to a weakening of surrounding ligaments that can cause dislocation of the toe."
+        "description": "Capsulitis is a condition in which these ligaments have become inflamed. Although capsulitis can also occur in the joints of the third or fourth toes, it most commonly affects the second toe."
       },
       {
         "name": "Sesamoid Injuries",
         "id": "sesamoid",
-        "description": "Sesamoid injuries can involve the bones, tendons and/or surrounding tissue in the joint. They are often associated with activities requiring increased pressure on the ball of the foot, such as running, basketball, football, golf, tennis and ballet. In addition, people with high arches are at risk for developing sesamoid problems."
+        "description": "Sesamoid injuries can involve the bones, tendons and/or surrounding tissue in the joint. They are often associated with activities requiring increased pressure on the ball of the foot. In addition, people with high arches are at risk for developing sesamoid problems."
       },
       {
         "name": "Calcaneal Apophysitis (Sever's Disease)",
         "id": "servers-disease",
-        "description": "It typically affects children between the ages of 8 and 14 years old, because the heel bone (calcaneus) is not fully developed until at least age 14. Until then, new bone is forming at the growth plate (physis), a weak area located at the back of the heel. When there is too much repetitive stress on the growth plate, inflammation can develop."
+        "description": "It typically affects children between the ages of 8 and 14 years old, because the heel bone (calcaneus) is not fully developed until at least age 14. When there is too much repetitive stress on the growth plate, inflammation can develop."
       },
       {
         "name": "Vitamin D Deficiency",
